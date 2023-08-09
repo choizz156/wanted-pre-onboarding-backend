48f83adf67993ccf071a10bfd1d4336e08063b07
choizz <tkfka156@gmail.com>
2023-08-09 17:20:18 +0900

    :recycle: refactor: 페이지네이션 size, page 파라미터 리팩토링

--- src/main/java/com/wanted/preonboarding/PostSearching.java
+++ src/main/java/com/wanted/preonboarding/PostSearching.java
@@ -12,17 +12,17 @@ import lombok.NoArgsConstructor;
 public class PostSearching {
 
     private static final int DEFAULT_SIZE = 10;
-    private static final int MAX_SIZE = 1000;
+    private static final int MAX_SIZE = 50;
     private static final int MIN_PAGE = 1;
     private static final Sort DEFAULT_SORT = Sort.DESC;
 
-    private int page ;
+    private int page;
     private Integer size;
     private Sort sort;
 
     private PostSearching(final int page, final Integer size, final Sort sort) {
         this.page = (page == 0 ? MIN_PAGE : page);
-        this.size = (size == null ? DEFAULT_SIZE : size);
+        this.size = getSize(size);
         this.sort = (sort == null ? Sort.DESC : Sort.ASC);
     }
 
@@ -32,5 +32,17 @@ public class PostSearching {
 
     public long getOffset() {
         return (long) (max(MIN_PAGE, page) - 1) * min(size, MAX_SIZE);
+    }
+
+    private int getSize(final Integer size) {
+        if (size == null || size < DEFAULT_SIZE) {
+            return DEFAULT_SIZE;
+        }
+
+        if (size > MAX_SIZE) {
+            return MAX_SIZE;
+        }
+
+        return size;
     }
 }
