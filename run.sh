e7e7a1ac4f6a059bb48c5beee93ef1b215b8de5e
choizz <tkfka156@gmail.com>
2023-08-09 17:55:58 +0900

    :truck: rename: docker 관련 디렉토리 생성 및 run.sh 설정 변경

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker-compose -p pre-onboarding-backend --env-file src/main/resources/config/.env  up -d
+docker-compose -p pre-onboarding-backend --env-file src/main/resources/config/.env -f \
+./docker/docker-compose.yml  up -d
