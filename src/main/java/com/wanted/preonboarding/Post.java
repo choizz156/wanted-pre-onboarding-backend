c4316fa850044af0549b978d6cd2d0c1a1e22a0c
choizz <tkfka156@gmail.com>
2023-08-10 22:27:21 +0900

    :recycle: refactor: content 타입 변경

--- src/main/java/com/wanted/preonboarding/Post.java
+++ src/main/java/com/wanted/preonboarding/Post.java
@@ -2,10 +2,12 @@ package com.wanted.preonboarding;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,7 +21,10 @@ public class Post extends BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     @ManyToOne
